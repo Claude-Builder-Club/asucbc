932212bf15609eae2df22e4194a5a589934e521d
Shiven Shekar <shivenshekar01@gmail.com>
2026-02-05 11:06:58 -0700

    Threw the big boys into the optimizer

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["nodemailer"],
+  experimental: {
+    optimizePackageImports: [
+      "react-icons",
+      "lucide-react",
+      "mathjs",
+      "framer-motion",
+    ],
+  },
 };
 
 export default nextConfig;
